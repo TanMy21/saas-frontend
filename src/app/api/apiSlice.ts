e41be97968ad0d3ff7f34e9fc859d35f050302be
TanMy21 <73067170+TanMy21@users.noreply.github.com>
2024-02-03 14:07:33 +0530

    persist v1

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -1,11 +1,65 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+import { setCredentials } from "../slices/authSlice";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:8686",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  // console.log(args) // request url, method, body
+  // console.log(api) // signal, dispatch, getState()
+  // console.log(extraOptions) //custom like {shout: true}
+
+  let result = await baseQuery(args, api, extraOptions);
+
+  // If you want, handle other status codes, too
+  if (result?.error?.status === 403) {
+    console.log("sending refresh token");
+
+    // send refresh token to get new access token
+    const refreshResult = await baseQuery("/refresh", api, extraOptions);
+
+    if (refreshResult?.data) {
+      // store the new token
+      api.dispatch(setCredentials({ ...refreshResult.data }));
+
+      // retry original query with new access token
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      if (refreshResult?.error?.status === 403) {
+        (refreshResult.error.data as { message: string }).message =
+          "Your login has expired. ";
+      }
+      return refreshResult;
+    }
+  }
+
+  return result;
+};
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8686",
-    credentials: "include",
-  }),
+  baseQuery: baseQueryWithReauth,
   tagTypes: ["User"],
   endpoints: (builder) => ({}),
 });
