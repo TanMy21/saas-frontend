a005ca8856a0b018440c1fa8edb9e4f221004ce0
TanMy21 <tanmay.gp.15@gmail.com>
2025-03-02 10:00:48 +0530

    depploy v1.01

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "./index.html",
+      output: {
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash].[ext]",
+      },
+    },
+  },
 });
